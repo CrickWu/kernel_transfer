1bfcdde1668e1a5d2b81d83e3bb428d477f2938a
Wei-Cheng Chang <wchang2@la.lti.cs.cmu.edu>
2016-04-24 21:02:33 -0400

    prefix path

---
+++ configure_path.py
@@ -0,0 +1,11 @@
+# source domain prefix path
+# trn set: .trn.libsvm
+# val set: .val.libsvm
+# tst set: .tst.libsvm
+srcPath = /usr0/home/wchang2/research/NIPS2016/data/binary/src.100
+
+# target domain prefix path
+tgtPath = /usr0/home/wchang2/research/NIPS2016/data/binary/tgt.40
+
+# parallel data prefix path
+prlPath = /usr0/home/wchang2/research/NIPS2016/data/parallel/
