fix gradient and modify dataloader

dataloader.py
@@ -7,11 +7,21 @@ from sklearn.datasets import load_svmlight_file
 from sklearn.metrics.pairwise import rbf_kernel
 from configure_path import *
 
+# keep the `nn` nearest neighbor for each row
+# nn is # nearest neighbor to keep
+def sparsify_K(K, nn):
+    ret_K = np.zeros(K.shape)
+    for i in xrange(K.shape[0]):
+        index = np.argsort(K[i, :])[-nn:]
+        ret_K[i, index] = K[i, index]
+    return ret_K
+
 # Load the data
 # return values:
 # y: (ns+nt): true_values
 # I: (ns+nt) observation indicator
 # K: (ns+nt) * (ns+nt), basic kernel, which could also be the i~j indicator
+# offset: [source_train , ... , target_para] offset number
 def load_data(source_train, source_test, source_para,
               target_train, target_test, target_para):
     source_n_features = 100000
@@ -20,13 +30,14 @@ def load_data(source_train, source_test, source_para,
     # source_domain, target_domain dimension should be fixed
 
     source_train_X, source_train_y = load_svmlight_file(source_train, n_features=source_n_features)
-    source_test_X, _ = load_svmlight_file(source_test, n_features=source_n_features)
+    source_test_X, source_test_y = load_svmlight_file(source_test, n_features=source_n_features)
     source_para_X, _ = load_svmlight_file(source_para, n_features=source_n_features, multilabel=True)
 
     target_train_X, target_train_y = load_svmlight_file(target_train, n_features=target_n_features)
-    target_test_X, _ = load_svmlight_file(target_test, n_features=target_n_features)
+    target_test_X, target_test_y = load_svmlight_file(target_test, n_features=target_n_features)
     target_para_X, _ = load_svmlight_file(target_para, n_features=target_n_features, multilabel=True)
-
+    ##### default gamma value is taken to be sqrt of the data dimension
+    ##### May need to tune and change the calculation of
     source_gamma = 1.0 / np.sqrt(source_train_X.shape[1])
     target_gamma = 1.0 / np.sqrt(target_train_X.shape[1])
 
@@ -41,8 +52,8 @@ def load_data(source_train, source_test, source_para,
     len_X = [source_train_X.shape[0] , source_test_X.shape[0] , source_para_X.shape[0]
             , target_train_X.shape[0] , target_test_X.shape[0] , target_para_X.shape[0]]
     offset = np.cumsum(len_X)
-    print offset
-    print len_X
+    print 'offset\t', offset
+    print '# instance ', len_X
 
     # K initialize
     n = offset[5]
@@ -62,10 +73,17 @@ def load_data(source_train, source_test, source_para,
     # true values
     y = np.zeros(n, dtype=np.float)
     y[0:offset[0]] = source_train_y
+    y[offset[0]:offset[1]] = source_test_y
     y[offset[2]:offset[3]] = target_train_y
+    y[offset[3]:offset[4]] = target_test_y
 
-    return y, I, K
-
+    return y, I, K, offset
+# Load the data
+# return values:
+# y: (ns+nt): true_values
+# I: (ns+nt) observation indicator
+# K: (ns+nt) * (ns+nt), basic kernel, which could also be the i~j indicator
+# offset: [source_train , ... , target_para] offset number
 def default_y_I_K():
     source_train = srcPath + '.trn.libsvm'
     source_test = srcPath + '.tst.libsvm'
@@ -75,9 +93,49 @@ def default_y_I_K():
     target_test = tgtPath + '.tst.libsvm'
     target_para = prlPath + 'tgt.toy.libsvm'
 
-    y, I, K = load_data(source_train, source_test, source_para,
+    y, I, K, offset = load_data(source_train, source_test, source_para,
                   target_train, target_test, target_para)
-    return y, I, K
+    return y, I, K, offset
+
+# Load the target [train,test] data (no parallel)
+# return values:
+# y: (ns+nt): true_values
+# I: (ns+nt) observation indicator
+# K: (ns+nt) * (ns+nt), basic kernel, which could also be the i~j indicator
+# offset: [train, test] offset
+def get_target_y_I_K(gamma=None):
+    target_train = tgtPath + '.trn.libsvm'
+    target_test = tgtPath + '.tst.libsvm'
+    target_n_features = 200000
+
+    target_train_X, target_train_y = load_svmlight_file(target_train, n_features=target_n_features)
+    target_test_X, target_test_y = load_svmlight_file(target_test, n_features=target_n_features)
+
+    len_X = [target_train_X.shape[0] , target_test_X.shape[0]]
+    offset = np.cumsum(len_X)
+
+    if gamma == None:
+        target_gamma = 1.0 / np.sqrt(target_train_X.shape[1]) # or target_data
+    else:
+        target_gamma = gamma
+
+    n = offset[1]
+
+    target_data = sp.vstack([target_train_X, target_test_X])
+    target_ker = rbf_kernel(target_data, gamma=target_gamma)
+
+    y = np.zeros(n, dtype=np.float)
+    y[0:offset[0]] = target_train_y
+    y[offset[0]:offset[1]] = target_test_y
+
+    I = np.zeros(n, dtype=np.float)
+    I[0:offset[0]] = np.ones(len_X[0], dtype=np.float)
+
+    K = target_ker
+    print 'offset\t', offset
+    return y, I, K, offset
+
+# for testing
 if __name__ == '__main__':
 
 

sgd.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 # encoding: utf-8
+# This file uses GD to update `U` and `f` simultaneously.
+# Note that the current alternating process does not
+# provide a good result due to either the bad approxmimation
+# of U or the slow convergance speed. Refer to full_data_semi.py
+# for a fixed `K` (kernel) and precise `f` solution.
 
 import numpy as np
 from joblib import Parallel, delayed
 from dataloader import default_y_I_K
 from sklearn.datasets import load_svmlight_file
+from sklearn.metrics import average_precision_score
 
 np.random.seed(123)
 if __name__ == '__main__':
@@ -39,9 +45,9 @@ if __name__ == '__main__':
             # if not i ~ j
             if K[i, j] == 0:
                 continue
-            coeff = 2 * K[i, j] * U[i,:].dot(U[j,:].T)
+            coeff = -2 * (K[i, j] - U[i,:].dot(U[j,:].T))
             grad += U[j,:] * coeff
-        return grad * w_2
+        return grad * w_3
 
     def grad_f():
         K_approx = U.dot(U.T)
@@ -50,21 +56,21 @@ if __name__ == '__main__':
         lap =  D - K_approx
         grad = I * (f - y) * 2
         # grad += np.asarray(lap.dot(f)).flatten() * 2
-        grad += lap.dot(f) * 2 * w_3
+        grad += lap.dot(f) * 2 * w_2
         return grad
 
 
-    y, I, K = default_y_I_K()
+    y, I, K, offset = default_y_I_K()
 
     d = 10
     n = len(y)
     # weighting coefficient for the second and third terms
-    w_2 = 1.0 / n
+    w_2 = 10.0 / n
     w_3 = 1.0 / n
 
     # lr: learning rate
-    lr_U = 1.0 / n * 1e4
-    lr_f = 1.0 / n * 1e4
+    lr_U = 1.0 / n * 1e0
+    lr_f = 1.0 / n * 1e0
 
     U = np.random.randn(n, d)
     # U = np.zeros([n, d])
@@ -74,31 +80,50 @@ if __name__ == '__main__':
     K_ind = np.zeros([n,n], dtype=np.float)
     K_ind[row_idx, col_idx] = 1.0
 
+    hist_grad_f = np.zeros(n)
     # doing optimization
     max_iter = 100
-    for iteration in xrange(max_iter):
-        loss = ((f - y)**2 * I).sum()
+    try:
+        for iteration in xrange(max_iter):
+            loss1 = ((f - y)**2 * I).sum()
 
-        K_approx = U.dot(U.T)
-        # D = np.diag( np.asarray(K_approx.sum(1)).flatten() )
-        D = np.diag( K_approx.sum(1) )
-        lap = D - K_approx
+            K_approx = U.dot(U.T)
+            # D = np.diag( np.asarray(K_approx.sum(1)).flatten() )
+            D = np.diag( K_approx.sum(1) )
+            lap = D - K_approx
 
-        loss += f.T.dot(lap).dot(f) / 2.0 * w_2
-        loss += ((U.dot(U.T) - K)**2 * K_ind).sum() * w_3
+            loss2 = f.T.dot(lap).dot(f) * w_2
+            loss3 = ((U.dot(U.T) - K)**2 * K_ind).sum() * w_3
+            loss = loss1+loss2+loss3
 
-        print 'at iteration', iteration, loss
-        def update_U(x):
-            return grad_U(x) * lr_U
+            print 'at iteration', iteration, loss1, loss2, loss3, loss
+            # target test offset range
+            start_offset = offset[3]
+            end_offset = offset[4]
+            print 'ap', average_precision_score( y[start_offset:end_offset], f[start_offset:end_offset], average='macro' )
+            def update_U(x):
+                return grad_U(x) * lr_U
 
-        ret_grad_U = Parallel(n_jobs=16, verbose=5)(delayed(update_U)(i) for i in xrange(n))
-        for i in xrange(n):
-            # U[i,:] -= ret_grad_U[i] * lr_U
-            U[i,:] -= ret_grad_U[i]
+            ret_grad_U = Parallel(n_jobs=16, verbose=5)(delayed(update_U)(i) for i in xrange(n))
+            for i in xrange(n):
+                # U[i,:] -= ret_grad_U[i] * lr_U
+                U[i,:] -= ret_grad_U[i]
 
-        f -=  grad_f() * lr_f
+            # project back, which ensures U*U^T 's entires are large than 0
+            U[U<0] = 0
 
-    with open('f.pred') as fout:
-        for f_ele, y_ele in zip(f, y):
-            fout.write('%g\t%g\n' % (f_ele, y_ele))
+            # f -=  grad_f() * lr_f
+            # adagrad
+            cur_grad_f = grad_f()
+            hist_grad_f += cur_grad_f ** 2
+            cur_grad_f = cur_grad_f / (1e-6 + np.sqrt(hist_grad_f))
+            f -= cur_grad_f * lr_f
+    except KeyboardInterrupt as e:
+        print 'Training interrupt'
+
+    np.savetxt('U.txt', U, fmt='%f')
+    np.savetxt('UUT.txt', U.dot(U.T), fmt='%f')
+    np.savetxt('K.txt', K, fmt='%f')
+
+    np.savetxt('f.pred', np.transpose([f, y, I]), fmt='%f')
 